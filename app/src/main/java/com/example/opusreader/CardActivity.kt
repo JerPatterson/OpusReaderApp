86734d6aa5b1362b08d40c1b22a8b2f9dd165e2f
JerPatterson <jeremie.patterson@polymtl.ca>
2023-12-24 12:31:25 -0500

    display the date as it should

--- app/src/main/java/com/example/opusreader/CardActivity.kt
+++ app/src/main/java/com/example/opusreader/CardActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.google.gson.Gson
+import java.util.Calendar
 
 class CardActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -16,16 +17,37 @@ class CardActivity : AppCompatActivity() {
 
         val gson = Gson()
         val card = gson.fromJson(intent.getStringExtra("card"), Card::class.java)
-        if (card != null) this.addCardInfo(card)
+        if (card != null) {
+            this.addCardInfoSection(card)
+        }
     }
 
-    private fun addCardInfo(card: Card) {
-        val title = findViewById<TextView>(R.id.cardSectionTitleTv)
+    private fun addCardInfoSection(card: Card) {
+        this.addCardInfoSectionTitles(card.type)
         val identifier = findViewById<TextView>(R.id.idValueTv)
-        val expirationDate = findViewById<TextView>(R.id.expiryDateValueTv)
+        val expiryDate = findViewById<TextView>(R.id.expiryDateValueTv)
 
-        title.text = if (card.type == CardType.Opus) "CARTE OPUS" else "CARTE OCCASIONNELLE"
         identifier.text = card.id.toString()
-        expirationDate.text = "Unknown"
+        val calendar = card.expiryDate
+        expiryDate.text = if (calendar != null) this.calendarToString(calendar) else "Aucune"
+    }
+
+    private fun addCardInfoSectionTitles(cardType: CardType) {
+        val type = findViewById<TextView>(R.id.cardSectionTitleTv)
+        val identifier = findViewById<TextView>(R.id.idTv)
+        val expirationDate = findViewById<TextView>(R.id.expiryDateTv)
+
+        type.text = if (cardType == CardType.Opus) "CARTE OPUS" else "CARTE OCCASIONNELLE"
+        identifier.text = "Identifiant"
+        expirationDate.text = "Date d'expiration"
+    }
+
+    private fun calendarToString(cal: Calendar): String {
+        return String.format(
+            "%04d-%02d-%02d",
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH).inc(),
+            cal.get(Calendar.DATE)
+        )
     }
 }
