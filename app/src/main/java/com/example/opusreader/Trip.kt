0518eca98f4a6d9ebd370d808eb8423641069076
JerPatterson <jeremie.patterson@polymtl.ca>
2024-04-01 20:52:42 -0400

    fix occasional evening next day trips showing prior to first use (might break other fares)

--- app/src/main/java/com/example/opusreader/Trip.kt
+++ app/src/main/java/com/example/opusreader/Trip.kt
@@ -8,4 +8,19 @@ class Trip(
     var operatorId: UInt,
     var useDate: Calendar,
     var firstUseDate: Calendar
-): Serializable
+): Serializable {
+    init {
+        val date = Calendar.getInstance()
+        if (firstUseDate.timeInMillis - useDate.timeInMillis > 1000) {
+            date.set(
+                useDate.get(Calendar.YEAR),
+                useDate.get(Calendar.MONTH),
+                useDate.get(Calendar.DATE) + 1,
+                useDate.get(Calendar.HOUR_OF_DAY),
+                useDate.get(Calendar.MINUTE),
+            )
+
+            useDate = date
+        }
+    }
+}
