753c88179917e36513e4eee2bddf58342446292b
JerPatterson <jeremie.patterson@polymtl.ca>
2025-01-03 09:37:47 -0500

    rename test directory according to new package name

--- app/src/androidTest/java/com/transition/ora/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/transition/ora/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.opusreader", appContext.packageName)
+        assertEquals("com.transition.ora", appContext.packageName)
     }
 }
