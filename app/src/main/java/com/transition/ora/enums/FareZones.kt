f1c43dcae467d1ad5f562305416421f2390df175
JerPatterson <jeremie.patterson@polymtl.ca>
2025-05-08 21:07:04 -0400

    add artm fare zone maps

---
+++ app/src/main/java/com/transition/ora/enums/FareZones.kt
@@ -0,0 +1,9 @@
+package com.transition.ora.enums
+
+enum class FareZones {
+    ARTM_A,
+    ARTM_AB,
+    ARTM_CD,
+    ARTM_ABC,
+    ARTM_ABCD,
+}
--- app/src/main/java/com/transition/ora/types/FareProduct.kt
+++ app/src/main/java/com/transition/ora/types/FareProduct.kt
@@ -1,7 +1,10 @@
 package com.transition.ora.types
 
+import com.transition.ora.enums.FareZones
+
 
 data class FareProduct(
     val name: String,
-    val descriptionStringId: Int
+    val descriptionStringId: Int,
+    val includedZones: FareZones? = null
 )
